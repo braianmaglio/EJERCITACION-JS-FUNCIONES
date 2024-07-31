d82d9ab83a44569a507a0dd52dff71f74c6505ed
Braian Maglio <braianmaglio76@gmail.com>
2024-07-31 19:34:52 -0300

    Se resuelve ejercicio04

--- ejercicio04.js
+++ ejercicio04.js
@@ -1,3 +1,12 @@
 // Conversión de minúscula a mayúscula:
-
+let caracter = 'c'
 // Escriba un programa en JavaScript que utilice una función que reciba un carácter como parámetro, si el carácter es una letra minúscula, modifique el parámetro para convertirlo en mayúscula.
+function minMay (caracter) {
+    if (caracter >= 'a' && caracter <= 'z') {
+        caracter = caracter.toUpperCase()
+        return caracter
+    }
+}
+minMay()
+
+console.log(minMay(caracter)) // 'A'
