f262bca2a6795fc17a4592f6866c37bb3be9ca8a
Braian Maglio <braianmaglio76@gmail.com>
2024-08-01 21:29:27 -0300

    Se resuelve ejercicio03

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,3 +1,9 @@
 // Función F(X, Y):
 
 // Haga un programa en JavaScript que llame a la función F(X, Y) = 5.6 * X + Y / 2.5 y que muestre los resultados para F(3, 5); F(9, 2) y F(56, 87).
+
+const f = (x, y) => 5.6 * x + y / 2.5
+
+console.log(f(3, 5))
+console.log(f(9, 2))
+console.log(f(56, 87))
