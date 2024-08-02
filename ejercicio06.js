e2197f7fca2208f49c1c3a3b570afbd2b9091f43
Braian Maglio <braianmaglio76@gmail.com>
2024-08-01 21:51:23 -0300

    Se resuelve ejercicio06

--- ejercicio06.js
+++ ejercicio06.js
@@ -1,3 +1,12 @@
 // Mayor de tres números:
-
 // Cree una función en JavaScript llamada MayorDeTres, que reciba tres números enteros y devuelva el valor del mayor de ellos. Por ejemplo, para los números 5, 7 y 5, devolvería el valor 7.
+const MayorDeTres = (a, b, c) =>{
+    if (a > b && a > c) {
+        console.log(a) 
+    } else if(b > a && b > c){
+        console.log(b);
+    } else if(c > a && c > b){
+        console.log(c);
+    }
+}
+MayorDeTres(5, 7, 15)
