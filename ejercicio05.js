dc4b8df4be27eac3e8424d02bb1175ff6c317851
Braian Maglio <braianmaglio76@gmail.com>
2024-08-01 21:44:46 -0300

    Se realiza ejercicio05

--- ejercicio05.js
+++ ejercicio05.js
@@ -1,3 +1,20 @@
 // Factorial de un número:
-
 // Escriba un programa en JavaScript con una función que reciba un valor entero positivo y devuelva su factorial.
+
+const prompt = require('prompt-sync')({ sigint: true })
+
+const n = parseInt(prompt('Ingrese un número entero positivo: '))
+
+const factorial = (n) => {
+    if (n === 0 || n === 1) return 1
+
+
+    for (let i = n - 1; i >= 1; i--){
+        n *= i;
+        //console.log(`factorial: `, N);
+    }
+    return n;
+}
+factorial(n)
+
+console.log(`El factorial de ${n} es: ${factorial(n)}`)
